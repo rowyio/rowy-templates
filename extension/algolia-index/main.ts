b3fa03f81069044b70418ac656c6cf67417fdccb
Bhavya Verma <vbhavya269@gmail.com>
2022-09-30 10:23:17 +0530

    Added Algolia Index template

---
+++ extension/algolia-index/main.ts
@@ -0,0 +1,10 @@
+const extensionBody: AlgoliaIndexBody = async ({ row, db, change, ref }) => {
+    // feel free to add your own code logic here
+
+    return ({
+        fieldsToSync: [],    // a list of string of column names
+        row: row,    // object of data to sync, usually the row itself
+        index: "",    // algolia index to sync to
+        objectID: ref.id,    // algolia object ID, ref.id is one possible choice
+    })
+}
